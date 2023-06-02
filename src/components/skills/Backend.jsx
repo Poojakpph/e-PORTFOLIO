ce59da22ca00e51f18568a063a341cb74f450a7f
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-06-02 15:00:35 +0530

    Update Backend.jsx

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -15,41 +15,9 @@ const Backend = () => {
                </div>
             </div>
 
-            <div className="skills__data"> 
-               <i class="uil uil-award-alt"></i> 
-               <div>
-                  <h3 className="skills__name">SQL</h3>
-                  <span className="skills__level">Intermediate</span>
-               </div>
-            </div>
-
-            <div className="skills__data"> 
-               <i class="uil uil-award-alt"></i> 
-               <div>
-                  <h3 className="skills__name">SQL</h3>
-                  <span className="skills__level">Intermediate</span>
-               </div>
-            </div>
-
         </div>
 
         <div className="skills__group">
-            <div className="skills__data"> 
-               <i class="uil uil-award-alt"></i> 
-               <div>
-                  <h3 className="skills__name">SQL</h3>
-                  <span className="skills__level">Intermediate</span>
-               </div>
-            </div>
-
-            <div className="skills__data"> 
-               <i class="uil uil-award-alt"></i> 
-               <div>
-                  <h3 className="skills__name">SQL</h3>
-                  <span className="skills__level">Intermediate</span>
-               </div>
-            </div>
-
             <div className="skills__data"> 
                <i class="uil uil-award-alt"></i> 
                <div>
